e89433f4b8f639eb7d25d9c6633274e74866c1bb
Cassidy Boilley <cboilley@my.bcit.ca>
2024-04-07 11:16:22 -0700

    test

--- api/index.js
+++ api/index.js
@@ -105,7 +105,8 @@ app.post('/login', async (req, res) => {
 
             res.cookie('token', token, { httpOnly: true, maxAge: 3600000 }); // Max age 1 hour
 
-            res.status(200).json({ message: 'Login successful', role: userRole, token }); // Include token in response
+          // res.status(200).json({ message: 'Login successful', role: userRole, token }); // Include token in response
+          res.redirect('./user.html')
         } else {
             res.status(401).json({ error: 'Invalid username or password' });
         }
