26ae4b473feb768d80fe7926698d648276f837bc
Cassidy Boilley <cboilley@my.bcit.ca>
2024-04-07 22:34:10 -0700

    COrs fix?

--- api/index.js
+++ api/index.js
@@ -214,7 +214,7 @@ app.post('/api-call', async (req, res) => {
 // PUT: Increment API call count
 app.put('/update-api-count', async (req, res) => {
   try {
-      res.setHeader('Access-Control-Allow-Origin', 'https://cassidyboilley-labs.netlify.app');
+      res.setHeader('Access-Control-Allow-Origin', '*');
         const { username } = req.body;
 
         // Find or create API call record for the user
